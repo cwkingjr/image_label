90a201708ce59605c5d6f454bfe5e5cd53d684d0
Chuck King <cwkingjr@gmail.com>
2017-06-09 21:09:53 -0500

    building location calculations

--- image_label.py
+++ image_label.py
@@ -5,6 +5,7 @@ import configparser
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+import sys
 
 config = configparser.ConfigParser()
 
@@ -27,9 +28,10 @@ colors = {
     'white' : (255,255,255)
 }
 
-config.read('config.ini')
+# acceptable location values
+locations = "N NE E SE S SW W NW".split()
 
-print(config.sections())
+config.read('config.ini')
 
 # validate all of these before production
 text = config['default']['text']
@@ -37,10 +39,42 @@ size = int(config['default']['size'])
 font_file = config['default']['font_file']
 offset = int(config['default']['offset'])
 color = config['default']['color']
+location = config['default']['location']
+
+# temp for testing
+location = 'NE'
+
+# validations
+if not location in locations:
+    print('Location choice of {} is not among acceptable values of {}'.format(location, locations))
+    sys.exit(1)
 
 img = Image.open("sample_in.jpg")
 draw = ImageDraw.Draw(img)
 font = ImageFont.truetype(font_file, size)
-# draw.text((x, y),"Sample Text",(r,g,b))
-draw.text((offset, 0), text, colors[color], font=font)
+
+width, height = img.size
+#print('image width', width)
+#print('image height', height)
+
+# TODO determine size of text to be printed and deduct that from x,y
+# to allow space for the text when on the right side of the drawing
+
+# establish print location x coordinate
+if location in ['NW', 'W', 'SW']:
+    x = offset
+elif location in ['N', 'S']:
+    x = width / 2
+elif location in ['NE', 'E', 'SE']:
+    x = width - offset
+
+# establish print location y coordinate
+if location in ['NW', 'N', 'NE']:
+    y = offset
+elif location in ['W', 'E']:
+    y = height / 2
+elif location in ['SW', 'S', 'SE']:
+    y = height - offset
+
+draw.text((x, y), text, colors[color], font=font)
 img.save('sample_out.jpg')
