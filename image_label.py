b4a746c9f78ec111d80f484ea79219c3dd681f8d
Chuck King <cwkingjr@gmail.com>
2017-07-01 18:11:53 -0500

    resolve lint warnings fixes #11

--- image_label.py
+++ image_label.py
@@ -6,68 +6,103 @@
 
 import argparse
 import configparser
+import os
+import sys
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 from PIL import ImageStat
-import os
-import sys
 
-config = configparser.ConfigParser()
+
+CONFIG = configparser.ConfigParser()
 
 # acceptable location values
 # top, top right, right, bottom right, etc
-locations = "T TR R BR B BL L TL".split()
+LOCATIONS = "T TR R BR B BL L TL".split()
 
 # RGB values for colors
-colors = {
-    'red' : (255,0,0),
-    'blue' : (0,0,255),
-    'green' : (0,153,0),
-    'yellow' : (255,255,0),
-    'black' : (0,0,0),
-    'white' : (255,255,255)
+COLORS = {
+    'red': (255, 0, 0),
+    'blue': (0, 0, 255),
+    'green': (0, 153, 0),
+    'yellow': (255, 255, 0),
+    'black': (0, 0, 0),
+    'white': (255, 255, 255)
 }
-acceptable_colors = sorted(colors.keys())
+ACCEPTABLE_COLORS = sorted(COLORS.keys())
 
 # http://pillow.readthedocs.io/en/3.1.x/handbook/image-file-formats.html
-output_formats = 'BMP JPEG PNG'.split()
+OUTPUT_FORMATS = 'BMP JPEG PNG'.split()
 
-parser = argparse.ArgumentParser(
-    description='Add a text label to an existing image. Optionally, also rotate the image.',
+PARSER = argparse.ArgumentParser(
+    description='Add a text label to an existing image. '
+                'Optionally, also rotate the image.',
     argument_default=argparse.SUPPRESS)
 
-parser.add_argument('-S','--setting', type=str, help='Name of section in config file to use.')
-parser.add_argument('-T','--label_text', type=str, help='Label text')
-parser.add_argument('-L','--label_location', choices=locations, help='Location of the label on the image.')
-parser.add_argument('-C','--font_color', choices=colors.keys(), help='Font color of the label text.')
-parser.add_argument('-F','--font_file', help='Path to TTF font file to use to generate the text label.')
-parser.add_argument('-Z','--font_size', type=int, help='Integer: Font size of the label text.')
-parser.add_argument('-H','--label_offset_LR', type=int, help='Integer: Pixels to offset the label from the left and right of the image.')
-parser.add_argument('-V','--label_offset_TB', type=int, help='Integer: Pixels to offset the label from the top and bottom of the image.')
-parser.add_argument('-R','--rotate', type=int, help='Integer: Degrees to rotate image clockwise [1-359].')
-parser.add_argument('-J','--jpg_quality', type=int, help='Integer: JPG quality [1-95]. Default is 93. Larger affects output file size but may not improve actual quality. Quality cannot get better than the original image.')
-parser.add_argument('-B','--black_for_BW', action='store_true', help='Use black as text color for black and white images. Default is white.')
-parser.add_argument('-I','--list_ini_sections', action='store_true', help='List the sections in your config file for convenience.')
-parser.add_argument('-O','--output_format', choices=output_formats, help='Output file format. Default is JPEG.')
+PARSER.add_argument('-S', '--setting',
+                    type=str,
+                    help='Name of section in config file to use.')
+PARSER.add_argument('-T', '--label_text',
+                    type=str,
+                    help='Label text')
+PARSER.add_argument('-L', '--label_location',
+                    choices=LOCATIONS,
+                    help='Location of the label on the image.')
+PARSER.add_argument('-C', '--font_color',
+                    choices=COLORS.keys(),
+                    help='Font color of the label text.')
+PARSER.add_argument('-F', '--font_file',
+                    help='Path to TTF font file to use to generate the text label.')
+PARSER.add_argument('-Z', '--font_size',
+                    type=int,
+                    help='Integer: Font size of the label text.')
+PARSER.add_argument('-H', '--label_offset_LR',
+                    type=int,
+                    help='Integer: Pixels to offset the label from the left '
+                    'and right of the image.')
+PARSER.add_argument('-V', '--label_offset_TB',
+                    type=int,
+                    help='Integer: Pixels to offset the label from the top '
+                    'and bottom of the image.')
+PARSER.add_argument('-R', '--rotate',
+                    type=int,
+                    help='Integer: Degrees to rotate image clockwise [1-359].')
+PARSER.add_argument('-J', '--jpg_quality',
+                    type=int,
+                    help='Integer: JPG quality [1-95]. Default is 93. Larger '
+                    'affects output file size but may not improve actual '
+                    'quality. Quality cannot get better than the original image.')
+PARSER.add_argument('-B', '--black_for_BW',
+                    action='store_true',
+                    help='Use black as text color for black and white images. '
+                    'Default is white.')
+PARSER.add_argument('-I', '--list_ini_sections',
+                    action='store_true',
+                    help='List the sections in your config file for convenience.')
+PARSER.add_argument('-O', '--output_format',
+                    choices=OUTPUT_FORMATS,
+                    help='Output file format. Default is JPEG.')
 
-args, files = parser.parse_known_args()
+ARGS, FILES = PARSER.parse_known_args()
 
 # get dict from Namespace object
-parsed = vars(args)
+PARSED = vars(ARGS)
 
 if 'IMAGE_LABEL_CONFIG' in os.environ:
-    config.read(os.environ['IMAGE_LABEL_CONFIG'])
+    CONFIG.read(os.environ['IMAGE_LABEL_CONFIG'])
 else:
-    print('Could not find environment variable of IMAGE_LABEL_CONFIG so could not read config file')
+    print('Could not find environment variable of IMAGE_LABEL_CONFIG '
+          'so could not read config file')
 
-if 'list_ini_sections' in parsed:
-    print(' '.join(sorted(config.sections())))
+if 'list_ini_sections' in PARSED:
+    print(' '.join(sorted(CONFIG.sections())))
     sys.exit(0)
 
-# using function object to host options as attributes instead of dict k:v
+
 def options():
+    """function object to host options attributies"""
     pass
+
 
 # set the internal default config options
 options.font_color = 'red'
@@ -82,7 +117,10 @@ options.jpg_quality = 93
 options.black_for_BW = False
 options.output_format = 'JPEG'
 
+
 def load_settings_values(mydict):
+    """update settings from new dict"""
+
     if 'font_color' in mydict:
         options.font_color = mydict['font_color']
     if 'font_file' in mydict:
@@ -106,29 +144,33 @@ def load_settings_values(mydict):
     if 'output_format' in mydict:
         options.output_format = mydict['output_format']
 
+
 # if the config file has a DEFAULTS section, override the internal
 # defaults with those settings
-if 'DEFAULTS' in config:
-    load_settings_values(config['DEFAULTS'])
+if 'DEFAULTS' in CONFIG:
+    load_settings_values(CONFIG['DEFAULTS'])
 
 # if setting option provided via CLI, override the current options
 # with the values from that section
-if 'setting' in parsed:
-    mysection = parsed['setting']
-    if mysection in config:
-        load_settings_values(config[mysection])
+if 'setting' in PARSED:
+    MYSECTION = PARSED['setting']
+    if MYSECTION in CONFIG:
+        load_settings_values(CONFIG[MYSECTION])
     else:
-        print('ERROR: --setting value of {} is not included in the config file'.format(mysection))
+        print('ERROR: --setting value of {} is not included in the config'
+              ' file'.format(MYSECTION))
 
-# override the current options with any CLI options
-load_settings_values(parsed)
+# override the current options with any additional CLI options
+load_settings_values(PARSED)
 
-if not options.font_color in acceptable_colors:
-    print('Font color choice of {} is not among acceptable values of {}'.format(options.font_color, acceptable_colors))
+if options.font_color not in ACCEPTABLE_COLORS:
+    print('Font color choice of {} is not among acceptable values of {}'
+          .format(options.font_color, ACCEPTABLE_COLORS))
     sys.exit(1)
 
-if not options.label_location in locations:
-    print('Location choice of {} is not among acceptable values of {}'.format(options.label_location, locations))
+if options.label_location not in LOCATIONS:
+    print('Location choice of {} is not among acceptable values of {}'
+          .format(options.label_location, LOCATIONS))
     sys.exit(1)
 
 if options.rotate:
@@ -137,48 +179,59 @@ if options.rotate:
         sys.exit(1)
 
 if not 1 <= options.jpg_quality <= 95:
-    print('jgp_quality must be between 1-95. This will affect image quality and output jpg/jpeg file size')
+    print('jgp_quality must be between 1-95. This will affect image quality '
+          'and output jpg/jpeg file size')
     sys.exit(1)
 
 # make sure entries from setting file are correct and converted to bool
-if type(options.black_for_BW) == str:
+if isinstance(options.black_for_BW, str):
     if options.black_for_BW == 'True':
         options.black_for_BW = True
     elif options.black_for_BW == 'False':
         options.black_for_BW = False
     else:
-        print('ERROR: When setting black_for_BW via config file, it must be set to True or False')
+        print('ERROR: When setting black_for_BW via config file,'
+              ' it must be set to True or False')
         sys.exit(1)
 
-if not options.output_format in output_formats:
-    print('ERROR: Output format {} is not valid, choose from {}'.format(options.output_format, output_formats))
+if options.output_format not in OUTPUT_FORMATS:
+    print('ERROR: Output format {} is not valid, choose from {}'
+          .format(options.output_format, OUTPUT_FORMATS))
     sys.exit(1)
 
 # test ttf file to make sure it's a ttf file using magic bytes
-true_type_font_magic = b'\x00\x01\x00\x00'
-with open(options.font_file,'rb') as f:
-    bites = f.read(4)
-    if bites != true_type_font_magic:
+TRUE_TYPE_FONT_MAGIC = b'\x00\x01\x00\x00'
+with open(options.font_file, 'rb') as f:
+    MAGIC_BYTES = f.read(4)
+    if MAGIC_BYTES != TRUE_TYPE_FONT_MAGIC:
         print('The font file provided does not appear to be a True Type Font file')
         sys.exit(1)
+
 
 # https://stackoverflow.com/questions/20068945/
 # detect-if-image-is-color-grayscale-or-black-and-white-with-python-pil
 def is_black_and_white(myimg):
-    # choosing 200 because I had some color images that only had ~500 v
-    COLOR_VARIANCE_THRESHOLD = 200
+    """Determine if image is black and white based on color variance"""
 
-    v = ImageStat.Stat(myimg).var
-    maxmin = abs(max(v) - min(v))
-    
-    if len(v) == 1:
+    # choosing 200 because I had some color images that only had ~500 v
+    color_variance_threshold = 200
+
+    variance_tuple = ImageStat.Stat(myimg).var
+    maxmin = abs(max(variance_tuple) - min(variance_tuple))
+
+    if len(variance_tuple) == 1:
+        # greyscale images only return a one-item tuple
         return True
-    elif maxmin < COLOR_VARIANCE_THRESHOLD:
+    elif maxmin < color_variance_threshold:
+        # otherwise you are guessing based on variance
         return True
     else:
         return False
-    
+
+
 def process_file(filename):
+    """Make file modifications as needed and save"""
+
     img = Image.open(filename)
 
     if options.rotate:
@@ -192,28 +245,30 @@ def process_file(filename):
     text_width, text_height = draw.textsize(options.label_text, font=font)
 
     if options.label_offset_LR * 2 + text_width > image_width:
-        print('label_offset_LR * 2, plus text width is too large to fit on {}'.format(filename))
+        print('label_offset_LR * 2, plus text width is too large to fit on {}'
+              .format(filename))
         sys.exit(1)
-    
+
     if options.label_offset_TB * 2 + text_height > image_height:
-        print('label_offset_TB * 2, plus text height is too large to fit on {}'.format(filename))
+        print('label_offset_TB * 2, plus text height is too large to fit on {}'
+              .format(filename))
         sys.exit(1)
 
     # establish print location x
     if options.label_location in ['TL', 'L', 'BL']:
-        x = options.label_offset_LR
+        x_coord = options.label_offset_LR
     elif options.label_location in ['T', 'B']:
-        x = (image_width - text_width) / 2
+        x_coord = (image_width - text_width) / 2
     elif options.label_location in ['TR', 'R', 'BR']:
-        x = image_width - (text_width + options.label_offset_LR)
+        x_coord = image_width - (text_width + options.label_offset_LR)
 
     # establish print location y
     if options.label_location in ['TL', 'T', 'TR']:
-        y = options.label_offset_TB
+        y_coord = options.label_offset_TB
     elif options.label_location in ['L', 'R']:
-        y = (image_height - text_height) / 2
+        y_coord = (image_height - text_height) / 2
     elif options.label_location in ['BL', 'B', 'BR']:
-        y = image_height - (text_height + options.label_offset_TB)
+        y_coord = image_height - (text_height + options.label_offset_TB)
 
     if is_black_and_white(img):
         # when the image is black and white, draw.text must provide a single color,
@@ -225,14 +280,18 @@ def process_file(filename):
             bwcolor = 255
             bwcolortext = 'white'
 
-        print('Image appears to be black and white, which cannot take RGB text, so using {} as text color'.format(bwcolortext))
-        draw.text((x, y), options.label_text, bwcolor, font=font)
+        print('Image appears to be black and white, which cannot take RGB text'
+              ', so using {} as text color'.format(bwcolortext))
+        draw.text((x_coord, y_coord), options.label_text, bwcolor, font=font)
     else:
-        draw.text((x, y), options.label_text, colors[options.font_color], font=font)
+        draw.text((x_coord, y_coord),
+                  options.label_text,
+                  COLORS[options.font_color],
+                  font=font)
 
     # add 'altered' to filename to prevent overwriting original
     if '.' in os.path.basename(filename):
-        path,_ = os.path.splitext(filename)
+        path, _ = os.path.splitext(filename)
         outfilename = "{}.altered.{}".format(path, options.output_format)
     else:
         outfilename = "{}.altered".format(path)
@@ -240,5 +299,6 @@ def process_file(filename):
     # of BMP JPEG PNG only JPEG consumes the 'quality' parameter
     img.save(outfilename, format=options.output_format, quality=options.jpg_quality)
 
-for filename in files:
+
+for filename in FILES:
     process_file(filename)
